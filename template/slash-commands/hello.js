9e679a3cddc8d40a103468e5865c0f737478b550
kinfkong <kinfkong@126.com>
2018-01-14 15:56:48 +0800

    fix

--- template/slash-commands/hello.js
+++ template/slash-commands/hello.js
@@ -3,7 +3,7 @@
  */
 
 /**
- * This module is the demo command for showing the image message.
+ * This module is showing the demo 'Hello World' command.
  *
  * @author TCSCODER
  * @version 1.0
@@ -14,18 +14,60 @@ const slack = require('../slack-framework');
 const commands = slack.commands;
 
 const command = commands.addCommand({
+  // the name of the command
+  // This name should be the same name as you configured in the Slash Command, including the '/' character.
   name: '/hello',
+
+  // the slash command handler
   handler: (req, res) => {
-    const message = res.createMessage(`Hello World! Are you admin? ${req.userInfo.is_admin ? 'Yes' : 'No'}`);
-    message.addImage({
-      title: 'sample image',
-      text: 'this is the sample image',
-      image_url: 'https://c1.staticflickr.com/6/5601/15459954198_25943c4242_z.jpg',
-      thumb_url: 'https://c1.staticflickr.com/7/6159/6207023756_bbca3b9025_m.jpg',
-    });
+    // req has the following fields
+    // - team_id
+    // - team_domain
+    // - channel_id
+    // - user_id
+    // - user_name
+    // - command
+    // - text
+    // - userInfo (only present when scopes: ['users:read'] is configured.)
+    // ---- id
+    // ---- team_id
+    // ---- name
+    // ---- real_name
+    // ---- is_admin
+    // ---- is_owner
+    // ---- is_primary_owner
+    // ---- is_bot
+    // ---- is_app_user
+    const message = res.createMessage(`Hello World! Welcome ${req.user_name}!`);
+    res.send(message);
+
+    // for more complicated message, you can view the README.md or the example/slash-commands
+  },
+
+  // optional, if scope 'users:read' is set, it will get the userInfo via slack API.
+  scopes: ['users:read'],
+
+  // optional. it can be 'in_channel' or 'ephemeral'. default: 'ephemeral'.
+  // 'ephemeral' sends the message only for the user executing the command, while 'in_channel' sends the message
+  // to all the users in the channel.
+  // in the message, you can use message.setResponseType(...); to override this setting.
+  responseType: 'in_channel',
+
+  // optional. this text will display when calling: /<command-name> help
+  // You should use either helpText or helpHandler.
+  // if helpText and helpHandler is not set, the help action will not be functioned.
+  // Note: the property will be ignored if helpHandler is also set.
+  helpText: 'This command is a hello world demo.',
+
+  // optional. this handler let's you to define a more complicated help message to replace helpText.
+  // You should use either helpText or helpHandler.
+  // if helpText and helpHandler is not set, the help action will not be functioned.
+  helpHandler: (req, res) => {
+    const message = res.createMessage('Thanks for using /hello command.');
+    message.addText('This is just a demo');
     res.send(message);
   },
-  scopes: ['users:read'],
+
 });
 
 
